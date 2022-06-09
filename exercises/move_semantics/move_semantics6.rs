d5d22dfbee3e13553b4534b47ecfa25d22b69cd6
maxlestage <maxlestage@icloud.com>
2022-06-09 20:44:49 +0200

    Exercices Semantics : ✅✅✅✅✅✅✅✅

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,6 +5,39 @@
 // I AM NOT DONE
 
 fn main() {
+    // let data = "Rust is great!".to_string();
+    // Je rends la variable mutable :
+    let mut data = "Rust is great!".to_string();
+
+    // get_char(data);
+    // Pour modifier la variable depuis la référance, je dois ajouter une référence à data et lui dire quelle est mutable
+    // et je dois le faire sur le type attendu de la function fn get_char(data: &String) -> char {..
+    get_char(&data);
+
+    // string_uppercase(&data);
+    // data est mutable donc on l'envoie vers la fonction fn string_uppercase(mut data: String) {.. directment
+    string_uppercase(data);
+}
+
+// Should not take ownership
+// fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+// fn string_uppercase(mut data: String) {
+// ici on rappel que la valeur d'entrée de data est mutable et on peut alors modifier sa valeur.
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
+
+    println!("{}", data);
+}
+
+/*
+
+Avant :
+fn main() {
     let data = "Rust is great!".to_string();
 
     get_char(data);
@@ -12,14 +45,17 @@ fn main() {
     string_uppercase(&data);
 }
 
-// Should not take ownership
+/// Should not take ownership
 fn get_char(data: String) -> char {
     data.chars().last().unwrap()
 }
 
-// Should take ownership
+/// Should take ownership
 fn string_uppercase(mut data: &String) {
     data = &data.to_uppercase();
 
     println!("{}", data);
 }
+
+
+*/
