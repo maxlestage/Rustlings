9a8b361f14e37aee67447e62f80ed0fc7e29b003
maxlestage <maxlestage@icloud.com>
2022-06-21 18:24:53 +0200

    Exercices Primitive types : 3 Refait d'une autre façon.

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,6 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
+// I AM NOT DONE
+/*
 fn main() {
     let a = [
         "eded",
@@ -113,5 +115,38 @@ fn main() {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
+ */
+
+fn main() {
+    // Init le Vec :
+    let mut a: Vec<&str> = vec![];
+
+    // Je passe la variable a à la function :
+    push_in_array(&mut a);
+
+    // Condition qui verifie si a est supp à 100 ou non :
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
+
+// Function qui me permet d'ajouter des éléments dans la variable a :
+fn push_in_array(a: &mut Vec<&str>) {
+    // Défini un index i qui vaut 0 :
+    let mut i: i32 = 0;
+
+    // Tant que i vaut moins de 100 :
+    while i < 100 {
+        // je push "test" dans a :
+        &a.push("test");
+        // j'affiche la longeure du vecteur a à ce moment :
+        println!("{}", a.len());
+
+        // J'incrémente i + 1 :
+        i += 1;
     }
 }
